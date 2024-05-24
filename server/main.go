df78a520635f0273d3185270f51eacb9ecedcf86
sakthi_l_u_c_i <81083482+sakthi-lucia0567@users.noreply.github.com>
2024-05-24 15:23:58 +0530

    fix: cors url

--- server/main.go
+++ server/main.go
@@ -21,7 +21,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:5173/",
+		AllowOrigins: "https://go-fiber-todo.vercel.app/",
 		AllowHeaders: "Origin, Content-Type, Accept",
 		AllowMethods: "POST,GET,PUT,PATCH,DELETE",
 	}))
